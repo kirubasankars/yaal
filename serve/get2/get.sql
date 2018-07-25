13bde679ee4040f3bf4a5b9bb3dea528faa8d9de
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-07-25 09:30:17 -0400

    files

---
+++ serve/get2/get.sql
@@ -0,0 +1,5 @@
+SELECT 1 as order_id, '5/5/2018' as order_date, 1 as item_id, 1 as qty, 1 as product_id, 'Apple' as product_name
+union all
+SELECT 1 as order_id, '5/5/2018' as order_date, 2 as item_id, 1 as qty, 2 as product_id, 'Orange' as product_name
+union all
+SELECT 2 as order_id, '5/5/2018' as order_date, 3 as item_id, 1 as qty, 3 as product_id, 'Pineapple' as product_name
---
+++ serve/get3/get.sql
@@ -0,0 +1,3 @@
+SELECT 1 as order_id, '5/7/2018' as order_date
+union all
+SELECT 2 as order_id, '5/7/2018' as order_date;
