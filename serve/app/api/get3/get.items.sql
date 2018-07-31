eae707184e1f8a0f6467c747723daf25f6dfc7f8
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-07-31 08:34:12 -0400

    with parameters and web server

---
+++ serve/app/api/get3/get.items.sql
@@ -0,0 +1,8 @@
+--($parent.id)--
+
+select * from (SELECT 1 as item_id, 1 as qty, 1 as product_id, 'Apple' as product_name, 1 as order_id
+union all
+SELECT 2 as item_id, 1 as qty, 2 as product_id, 'Orange' as product_name, 1 as order_id
+union all
+SELECT 3 as item_id, 1 as qty, 3 as product_id, 'Pineapple' as product_name, 2 as order_id) 
+where order_id = {{$parent.id}}
