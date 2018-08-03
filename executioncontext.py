70b29c7ddde64f349b53eb42dc76f01f1ccea3d1
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-08-02 23:39:16 -0400

    Align execution context and descriptor interfaces

--- executioncontext.py
+++ executioncontext.py
@@ -8,10 +8,7 @@ def _dict_factory(cursor, row):
     return d
 
 class SQLiteExecutionContext:
-
-    def __init__(self):
-        self._parameter_rx = re.compile("\{\{([A-Za-z0-9_.$-]*?)\}\}", re.MULTILINE)
-
+        
     def begin(self):
         pass
 
@@ -23,10 +20,13 @@ class SQLiteExecutionContext:
 
     def execute(self, node_executor, input_shape):        
         node_descriptor = node_executor.get_node_descritor()
+        
         content = node_descriptor.get_content()
         if content is None:
             return []
-        content = self._parameter_rx.sub("?", content)                    
+        
+        content = node_descriptor.get_content_replaced_parameters("?")                    
+        
         con = lite.connect("chinook.db")
         con.row_factory = _dict_factory    
         with con:
