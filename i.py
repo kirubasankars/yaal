9ad6982dc5429f1c4b14170c560856cd9f133039
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-08-24 06:19:56 -0400

    timeit

---
+++ i.py
@@ -0,0 +1,23 @@
+# importing the required module
+import timeit
+ 
+# code snippet to be executed only once
+mysetup = "from gravity import Gravity, create_input_shape, get_result"
+ 
+# code snippet whose execution time is to be measured
+mycode = '''
+path = "api/film"
+method = "get"
+root_path = "serve/pos"
+
+app = Gravity(root_path, None)
+node_descriptor = app.create_descriptor(method, path, False)
+execution_contexts = app.create_execution_contexts()
+input_shape = create_input_shape(node_descriptor, None, None, { "page" : 1 }, None)
+get_result(node_descriptor, execution_contexts, input_shape)
+'''
+ 
+# timeit statement
+print(timeit.timeit(setup = mysetup,
+                    stmt = mycode,
+                    number = 10))
