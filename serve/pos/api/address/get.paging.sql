e8b72ad7b116f42c5e89ab2a1ef2c3dcc4939961
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-08-25 16:50:24 -0400

    cleanup

--- serve/pos/api/address/get.paging.sql
+++ serve/pos/api/address/get.paging.sql
@@ -1,9 +1,9 @@
 --($query.page integer)--
 
---query()(sqlite3)--
+--query(sqlite3)--
 
 select 1 as "$error", 'page can''t be less then 1.' as "message" where {{$query.page}} < 1
 
---query()()--
+--query()--
 
 SELECT {{$query.page}} as current_page, ceil(count(*) / 10::float) as total_pages FROM address a join city c on a.city_id = c.city_id
