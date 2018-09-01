a767dc944ac0164d59a7b842d76509523cb3845d
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-08-31 20:36:50 -0400

    better names

---
+++ serve/pos/api/film/before.get.sql
@@ -0,0 +1,11 @@
+--($query.page integer)--
+
+SELECT 
+    * 
+FROM 
+    (select * from film order by film_id limit 10 offset ({{$query.page}} - 1) * 10) f 
+LEFT JOIN
+    film_actor fa ON fa.film_id = f.film_id
+LEFT JOIN 
+    actor a ON fa.actor_id = a.actor_id
+order by f.film_id
