3f615c7e56577a8d320b3e9377def4d2a51e71a7
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-09-05 20:12:09 -0400

    Update tests for $-named descriptor part paths

--- test_action_output.py
+++ test_action_output.py
@@ -1,5 +1,5 @@
 import unittest
-from gravity import _get_actions_output, create_context
+from gravity import _get_leafs_output, create_context
 
 class FakeExecutionContext:
     
@@ -12,20 +12,20 @@ class FakeExecutionContext:
     def error(self):
         pass
 
-    def execute(self, descriptor, input_shape, helper):
-        if descriptor["content"] == "error":
+    def execute(self, trunk, input_shape, helper):
+        if trunk["content"] == "error":
             return [{ "$http_status_code" : 400 , "$error" : 1, "message" : "message1" }, { "$error" : 1, "message" : "message2", '$http_status_code': 400 }], 0
 
-        if descriptor["content"] == "cookie":
+        if trunk["content"] == "cookie":
             return [{ "$cookie" : 1, "name" : "name1", "value" : "value" }, { "$cookie" : 1, "name" : "name2", "value" : "value" }], 0
 
-        if descriptor["content"] == "header":
+        if trunk["content"] == "header":
             return [{ "$header" : 1, "name" : "name1", "value" : "value" }, { "$header" : 1, "name" : "name2", "value" : "value" }], 0
 
-        if descriptor["content"] == "params":
+        if trunk["content"] == "params":
             return [{ "$params" : 1, "a" : "1", "b" : "2" }], 1
 
-        if descriptor["content"] == "break":
+        if trunk["content"] == "break":
             return [{ "$break" : 1, "a" : "1", "b" : "2" }], 1
 
 class TestGravity(unittest.TestCase):
@@ -36,9 +36,9 @@ class TestGravity(unittest.TestCase):
     def tearDown(self):
         pass
         
-    def test_descriptor_with_error(self):
-        descriptor = {
-            "actions" : [
+    def test_trunk_with_error(self):
+        trunk = {
+            "leafs" : [
                 {
                     "content" : "error"
                 }
@@ -48,20 +48,20 @@ class TestGravity(unittest.TestCase):
             "_validators" : None
         }
         
-        ctx = create_context(descriptor, "user", None, None, None, None, None, None)
+        ctx = create_context(trunk, "user", None, None, None, None, None, None)
 
         p = {
             "db" : FakeExecutionContext()
         }
 
-        rs, errors = _get_actions_output(descriptor, p, ctx, None)
+        rs, errors = _get_leafs_output(trunk, p, ctx, None)
 
         self.assertListEqual(errors, [{ "$error" : 1, "message" : "message1", '$http_status_code': 400 }, { "$error" : 1, "message" : "message2", '$http_status_code': 400 }])
         self.assertEqual(ctx.get_prop("$response.status_code"), 400)
 
-    def test_descriptor_with_cookie(self):
-        descriptor = {
-            "actions" : [
+    def test_trunk_with_cookie(self):
+        trunk = {
+            "leafs" : [
                 {
                     "content" : "cookie"
                 }                
@@ -71,19 +71,19 @@ class TestGravity(unittest.TestCase):
             "_validators" : None
         }
         
-        ctx = create_context(descriptor, "user", None, None, None, None, None, None)
+        ctx = create_context(trunk, "user", None, None, None, None, None, None)
 
         p = {
             "db" : FakeExecutionContext()
         }
 
-        _get_actions_output(descriptor, p, ctx, None)
+        _get_leafs_output(trunk, p, ctx, None)
 
         self.assertDictEqual(ctx.get_prop("$response.$cookie").get_data(), {'name1': {'$cookie': 1, 'name': 'name1', 'value': 'value'}, 'name2': {'$cookie': 1, 'name': 'name2', 'value': 'value'}})
 
-    def test_descriptor_with_header(self):
-        descriptor = {
-            "actions" : [
+    def test_trunk_with_header(self):
+        trunk = {
+            "leafs" : [
                 {
                     "content" : "header"
                 }                
@@ -93,19 +93,19 @@ class TestGravity(unittest.TestCase):
             "_validators" : None
         }
         
-        ctx = create_context(descriptor, "user", None, None, None, None, None, None)
+        ctx = create_context(trunk, "user", None, None, None, None, None, None)
 
         p = {
             "db" : FakeExecutionContext()
         }
 
-        _get_actions_output(descriptor, p, ctx, None)
+        _get_leafs_output(trunk, p, ctx, None)
 
         self.assertDictEqual(ctx.get_prop("$response.$header").get_data(), {'name1': {'$header': 1, 'name': 'name1', 'value': 'value'}, 'name2': {'$header': 1, 'name': 'name2', 'value': 'value'}})
 
-    def test_descriptor_with_params(self):
-        descriptor = {
-            "actions" : [
+    def test_trunk_with_params(self):
+        trunk = {
+            "leafs" : [
                 {
                     "content" : "params"
                 }                
@@ -115,19 +115,19 @@ class TestGravity(unittest.TestCase):
             "_validators" : None
         }
         
-        ctx = create_context(descriptor, "user", None, None, None, None, None, None)
+        ctx = create_context(trunk, "user", None, None, None, None, None, None)
 
         p = {
             "db" : FakeExecutionContext()
         }
 
-        _get_actions_output(descriptor, p, ctx, None)
+        _get_leafs_output(trunk, p, ctx, None)
 
         self.assertDictEqual(ctx.get_prop("$params").get_data(), {'$last_inserted_id': 1, '$params': 1, 'a': '1', 'b': '2'})
 
-    def test_descriptor_with_break(self):
-        descriptor = {
-            "actions" : [
+    def test_trunk_with_break(self):
+        trunk = {
+            "leafs" : [
                 {
                     "content" : "break",
                     "connection" : "db"
@@ -138,20 +138,19 @@ class TestGravity(unittest.TestCase):
             "_validators" : None
         }
         
-        ctx = create_context(descriptor, "user", None, None, None, None, None, None)
+        ctx = create_context(trunk, "user", None, None, None, None, None, None)
 
         p = {
             "db" : FakeExecutionContext()
         }
 
-        rs, errors = _get_actions_output(descriptor, p, ctx, None)
+        rs, errors = _get_leafs_output(trunk, p, ctx, None)
 
         self.assertListEqual(rs, [{'a': '1', 'b': '2'}])
 
-    
-    def test_descriptor_with_connection_missing(self):
-        descriptor = {
-            "actions" : [
+    def test_trunk_with_connection_missing(self):
+        trunk = {
+            "leafs" : [
                 {
                     "content" : "break",
                     "connection" : "db1"
@@ -162,13 +161,13 @@ class TestGravity(unittest.TestCase):
             "_validators" : None
         }
         
-        ctx = create_context(descriptor, "user", None, None, None, None, None, None)
+        ctx = create_context(trunk, "user", None, None, None, None, None, None)
 
         p = {
             "db" : FakeExecutionContext()
         }
 
-        with self.assertRaises(Exception): _get_actions_output(descriptor, p, ctx, None)   
+        with self.assertRaises(Exception): _get_leafs_output(trunk, p, ctx, None)   
 
 if __name__ == "__main__":
     unittest.main()
