f11e984bc8e1cf92fd30179383d50e5177db29ef
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-09-12 06:58:48 -0400

    Reformat core Gravity and console code

--- gravity_console.py
+++ gravity_console.py
@@ -1,24 +1,11 @@
-import re, argparse, json
 
 from gravity import Gravity, create_context, get_result_json
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', help='path')
-    parser.add_argument('--method', help='method')
-    args = parser.parse_args()
 
-    args.path = "api/film"
-    args.method = "get"
-
-    if not (args.path or args.method):
-        parser.print_help()
-        exit() 
-    
+    path = "film/get"
     root_path = "serve/pos"
     app = Gravity(root_path, None, False)
-    descriptor = app.create_descriptor(args.method, args.path)
-    data_providers = app.get_data_providers()
-    context = create_context(descriptor, { "page" : 1 }, None, { "page" : 1 }, None, None)    
-    print(get_result_json(descriptor, data_providers, context))
-    #print(json.dumps(node_descriptor))
+    descriptor = app.create_descriptor(path)
+    context = create_context(descriptor, {"page": 1}, None, None, None, None, None, None)
+    print(get_result_json(descriptor, app.get_data_provider, context))
