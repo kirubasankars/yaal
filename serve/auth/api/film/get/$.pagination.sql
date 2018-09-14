230956b16299856e4acda148bb3d4861657b4474
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2018-09-13 21:05:25 -0400

    Restore lowercase film pagination descriptor

---
+++ serve/auth/api/film/get/$.pagination.sql
@@ -0,0 +1,30 @@
+--($query.page integer, $params.total_pages integer)--
+
+--query()--
+
+SELECT 
+    'params' as "$type",
+    (count(*) / 10::float) as total_pages
+FROM 
+    film
+
+--query(sqlite3)--
+
+SELECT 
+    'error' as "$type",
+    1 as code, 
+    "page can't less then 1 or more then " || {{$query.page}} as message
+WHERE 
+    {{$query.page}} < 1 or {{$query.page}} > {{$params.total_pages}} 
+
+--query(sqlite3)--
+
+SELECT
+    {{$query.page}} as current_page,
+    {{$params.total_pages}} as total_pages,
+    CASE WHEN {{$query.page}} < {{$params.total_pages}} THEN 
+        ("http://localhost:5000/_auth/api/film?page=" || ({{$query.page}} + 1))
+    ELSE
+        null
+    END as next_page
+    
