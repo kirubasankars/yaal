783d4095a304043c9d099af16cb11b1551161eea
Kiruba Sankar Swaminathan <kirubasankars@Kirubas-MBP.verizon.net>
2018-10-19 06:41:49 -0400

    test cases

--- test_yaal4.py
+++ test_yaal4.py
@@ -1,45 +1,29 @@
 import unittest
 
-from yaal import Yaal, create_context
-
-
-class FakeExecutionContext:
-
-    def begin(self):
-        pass
-
-    def end(self):
-        pass
-
-    def error(self):
-        pass
-
-    def execute(self, node_executor, input_shape):
-        return [input_shape._data], 0
-
-
-class FakeContentReader:
-
-    def get_sql(self, method, path):
-        return """--(id1, id2)--
-select {{id1}}
---query()--
-        """
-
-    def get_config(self, path):
-        return None
-
-    def list_sql(self, path):
-        return ["$"]
-
-    def get_routes_config(self, path):
-        return None
-
+from yaal import _to_lower_keys, _to_lower_keys_deep
 
 class TestGravity(unittest.TestCase):
     
-    def __init__(self):
-        pass
+    def test_simple_object_lower_keys(self):
+       obj = {"Name": "kiruba", "Age": 31}
+       self.assertDictEqual(_to_lower_keys(obj), {"name": "kiruba", "age": 31})
+
+    def test_complex_object_lower_keys(self):
+       obj = {"A": "a", "B": "B", "C": {"D":"d"}}
+       self.assertDictEqual(_to_lower_keys(obj), {"a":"a","b":"B","c":{"D":"d"}})
+
+    def test_complex_array_object_lower_keys(self):
+       obj = {"A": "a", "B": "B", "C": {"D":"d"},"Array":[{"F":"f"}]}
+       self.assertDictEqual(_to_lower_keys(obj), {"a": "a", "b": "B", "c": {"D":"d"},"array":[{"F":"f"}]})
+
+    def test_object_to_lower_keys_deep(self):
+       obj = {"A": "a", "B": "B", "C": {"D":"d"}}
+       self.assertDictEqual(_to_lower_keys_deep(obj), {"a":"a","b":"B","c":{"d":"d"}})
+
+    def test_complex_array_object_lower_keys_deep(self):
+       obj = {"A": "a", "B": "B", "C": {"D":"d"},"array":[{"F":"f"}]}
+       self.assertDictEqual(_to_lower_keys_deep(obj), {"a": "a", "b": "B", "c": {"d":"d"},"array":[{"f":"f"}]})
+
 
 if __name__ == "main":
     unittest.main()
